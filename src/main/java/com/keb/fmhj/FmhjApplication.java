dcc70565c5d17ad6b19f453dbf25b5d69926c1cb
MinSang22Kim <powerminsang9901@gmail.com>
2024-07-09 15:37:45 +0900

    chore: initialize-project-repository

---
+++ src/main/java/com/keb/fmhj/FmhjApplication.java
@@ -0,0 +1,13 @@
+package com.keb.fmhj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmhjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmhjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keb/fmhj/FmhjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keb.fmhj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmhjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
