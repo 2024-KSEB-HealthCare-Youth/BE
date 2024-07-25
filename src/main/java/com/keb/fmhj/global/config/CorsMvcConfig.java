f0d0e4b24339982a23b5da21ba30ef33c465488f
MinSang22Kim <powerminsang9901@gmail.com>
2024-07-25 17:20:23 +0900

    fix: swagger 및 security 설정 파일 추가 - #50

--- src/main/java/com/keb/fmhj/global/config/CorsMvcConfig.java
+++ src/main/java/com/keb/fmhj/global/config/CorsMvcConfig.java
@@ -9,12 +9,12 @@ public class CorsMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
-
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://localhost:8080")
-                .allowedOrigins("http://52.79.103.61:3000")
-                .allowedOrigins("http://52.79.103.61:8080");
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:8080",
+                        "http://52.79.103.61:3000",
+                        "http://52.79.103.61:8080"
+                );
     }
 }
-
