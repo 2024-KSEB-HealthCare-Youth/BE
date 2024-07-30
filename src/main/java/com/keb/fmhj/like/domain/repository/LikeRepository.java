435efa951c4f78b2a98fe6f00452ceb5253823e5
MinSang22Kim <powerminsang9901@gmail.com>
2024-07-30 17:15:35 +0900

    feat: Like API 기능 구현 - #53

--- src/main/java/com/keb/fmhj/like/domain/repository/LikeRepository.java
+++ src/main/java/com/keb/fmhj/like/domain/repository/LikeRepository.java
@@ -1,10 +1,15 @@
 package com.keb.fmhj.like.domain.repository;
 
 import com.keb.fmhj.like.domain.Like;
+import com.keb.fmhj.member.domain.Member;
+import com.keb.fmhj.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
+    Optional<Like> findByMemberAndPost(Member member, Post post);
 }
