30d51dbdfe39abb0f298f52a75d38ce639dfcdfb
MinSang22Kim <powerminsang9901@gmail.com>
2024-07-31 16:32:01 +0900

    refactor: 좋아요 코드 간소화 - #53

--- src/main/java/com/keb/fmhj/like/presentation/LikeController.java
+++ src/main/java/com/keb/fmhj/like/presentation/LikeController.java
@@ -3,12 +3,10 @@ package com.keb.fmhj.like.presentation;
 import com.keb.fmhj.auth.utils.AccessTokenUtils;
 import com.keb.fmhj.global.exception.ErrorCode;
 import com.keb.fmhj.global.response.ApiResponse;
-import com.keb.fmhj.like.domain.request.LikeRequestDto;
 import com.keb.fmhj.like.service.LikeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,22 +19,20 @@ public class LikeController {
 
     @PostMapping
     @Operation(summary = "좋아요 등록 API", description = "새로운 좋아요를 등록합니다.")
-    public ApiResponse<Void> addLike(@RequestParam Long postId,
-                                     @RequestBody LikeRequestDto likeDto) {
+    public ApiResponse<Void> addLike(@RequestParam Long postId) {
 
         String loginId = AccessTokenUtils.isPermission();
-        likeService.addLike(loginId, postId, likeDto);
+        likeService.addLike(loginId, postId);
         return new ApiResponse<>(ErrorCode.REQUEST_OK);
     }
 
     // 좋아요 삭제
     @DeleteMapping
     @Operation(summary = "좋아요 삭제 API", description = "좋아요를 삭제합니다.")
-    public ApiResponse<Void> deleteLike(@RequestParam Long postId,
-                                        @RequestBody LikeRequestDto likeDto){
+    public ApiResponse<Void> deleteLike(@RequestParam Long postId){
 
         String loginId = AccessTokenUtils.isPermission();
-        likeService.deleteLike(loginId, postId, likeDto);
+        likeService.deleteLike(loginId, postId);
         return new ApiResponse<>(ErrorCode.REQUEST_OK);
     }
 }
