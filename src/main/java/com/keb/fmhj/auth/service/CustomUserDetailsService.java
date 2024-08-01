050ac321e8473b5b1a9c7d23763ddce622b15ea9
MinSang22Kim <powerminsang9901@gmail.com>
2024-08-01 10:46:46 +0900

    refactor: 토큰 관련 오류 수정 - #82

--- src/main/java/com/keb/fmhj/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/keb/fmhj/auth/service/CustomUserDetailsService.java
@@ -1,15 +1,13 @@
 package com.keb.fmhj.auth.service;
 
 import com.keb.fmhj.auth.domain.CustomUserDetails;
-import com.keb.fmhj.member.domain.Member;
+import com.keb.fmhj.global.exception.ErrorCode;
+import com.keb.fmhj.global.exception.YouthException;
 import com.keb.fmhj.member.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,15 +16,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
-
-        Optional<Member> userData = memberRepository.findByLoginId(loginId);
-
-        if (userData.isEmpty()) {
-            throw new UsernameNotFoundException("해당 유저를 찾을 수 없습니다: " + loginId);
-            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
-        }
-
-        return new CustomUserDetails(userData.get());
+    public UserDetails loadUserByUsername(String loginId) {
+        return memberRepository.findByLoginId(loginId)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> YouthException.from(ErrorCode.MEMBER_NOT_FOUND));
     }
 }
