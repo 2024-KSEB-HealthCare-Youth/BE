284800439cc8b9b2caab577a02efed22b7306bed
Gyoung_0 <hkhj77777@gmail.com>
2024-08-02 15:42:56 +0900

    refactor: Like API modify

--- src/main/java/com/keb/fmhj/like/domain/repository/LikeRepository.java
+++ src/main/java/com/keb/fmhj/like/domain/repository/LikeRepository.java
@@ -1,8 +1,6 @@
 package com.keb.fmhj.like.domain.repository;
 
 import com.keb.fmhj.like.domain.Like;
-import com.keb.fmhj.member.domain.Member;
-import com.keb.fmhj.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
-    Optional<Like> findByMemberAndPost(Member member, Post post);
+    Optional<Like> findByMemberIdAndPost_PostId(long id, long postId);
 }
